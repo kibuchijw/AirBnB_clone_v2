dfbad225abc5b78a0f8b98f0bd1f5425e627350d
kibuchijw <kibuchijw@gmail.com>
2023-12-31 11:37:05 +0000

    Update (0-hello_route.py), add docstrings

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Starts a Flask web application.
+Listens on port 0.0.0.0, port 5000
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,7 +11,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 # Route definitions
 def index():
-    """ Returns Hello HBNB """
+    """ Displays Hello HBNB """
     return 'Hello HBNB!'
 
 
