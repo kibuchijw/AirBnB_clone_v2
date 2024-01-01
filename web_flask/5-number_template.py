611f72886183cf4a4deb6c7eb04c71416b0607da
kibuchijw <kibuchijw@gmail.com>
2024-01-01 10:28:38 +0000

    Update (5-number_template.py), to handle integer check

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -46,7 +46,9 @@ def show_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def show_number_template(n):
     """ Displays a HTML page only if n is an integer """
-    return render_template('number.html', n=n)
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+    return 'Not Found', 404
 
 
 if __name__ == '__main__':
